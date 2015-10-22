8a5c6a874b4023ed64edc2e1d310f7021754fadc
Weixin Wu <wuweixin@gmail.com>
2015-10-22 16:58:41 +0800

    Admin::UsersController

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20151022075501) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
