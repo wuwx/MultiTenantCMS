32db6ba6622d38769869c2a13d199246346a6aef
Weixin Wu <wuweixin@gmail.com>
2015-10-24 22:29:34 +0800

    need auth

--- app/controllers/sites/settings/application_controller.rb
+++ app/controllers/sites/settings/application_controller.rb
@@ -1,10 +1,3 @@
 class Sites::Settings::ApplicationController < ::ApplicationController
-  before_action :set_site
-
-  private
-    def set_site
-      @site ||= Site.find(params[:site_id])
-      add_breadcrumb @site.title, site_path(@site)
-      add_breadcrumb "控制面板", site_settings_path(@site)
-    end
+  before_action :authenticate_user!
 end
