ab80032cf9edbb4019e2ea65c3358582fb52fe3c
wuwx <wuweixin@gmail.com>
2015-10-27 11:35:07 +0800

    ThemesOnRails

---
+++ app/controllers/admin/themes_controller.rb
@@ -0,0 +1,2 @@
+class Admin::ThemesController < ApplicationController
+end
---
+++ test/controllers/admin/themes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::ThemesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
